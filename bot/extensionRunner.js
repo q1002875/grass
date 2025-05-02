f269e38d11d93542df22c37278f8ef1b8149f7cc
FullCorpJeff <57120894+FullCorpJeff@users.noreply.github.com>
2025-05-02 15:17:43 +0800

    grass3

--- bot/extensionRunner.js
+++ bot/extensionRunner.js
@@ -3,7 +3,6 @@ const puppeteer = require('puppeteer');
 async function launchWithExtension(proxy) {
     const browser = await puppeteer.launch({
         headless: "new",
-       // 使用你找到的 Chromium 路徑
         args: proxy ? [`--proxy-server=${proxy}`] : []
     });
     const page = await browser.newPage();
