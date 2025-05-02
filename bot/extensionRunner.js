673e7e753ff5e933dbdf866e10f6ce56e91312ef
FullCorpJeff <57120894+FullCorpJeff@users.noreply.github.com>
2025-05-02 15:53:50 +0800

    grass7

--- bot/extensionRunner.js
+++ bot/extensionRunner.js
@@ -1,17 +1,16 @@
-
 const puppeteer = require('puppeteer-core');
 
-async function launchWithExtension() {
+async function launchWithExtension(proxy) {
     const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: '/opt/render/.chromium/chromium',  // 更新為 Render 預設的 Chromium 路徑
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: "new",
+        executablePath: '/opt/render/.chromium/chromium',
+        args: proxy ? [`--proxy-server=${proxy}`] : []
     });
-
     const page = await browser.newPage();
     await page.goto('https://app.getgrass.io');
-    await page.waitForTimeout(5000);  // 或其他需要等待的時間
+    await new Promise(resolve => setTimeout(resolve, 5000));
     await browser.close();
 }
 
-launchWithExtension();
+module.exports = { launchWithExtension };
+ 
