7c5bcf655032912f8787e2b24d41141fd3985c1b
FullCorpJeff <57120894+FullCorpJeff@users.noreply.github.com>
2025-05-02 16:09:38 +0800

    grass9

--- bot/extensionRunner.js
+++ bot/extensionRunner.js
@@ -1,16 +1,21 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 async function launchWithExtension(proxy) {
-    const browser = await puppeteer.launch({
-        headless: "new",
-     
-        args: proxy ? [`--proxy-server=${proxy}`] : []
-    });
-    const page = await browser.newPage();
-    await page.goto('https://app.getgrass.io');
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    await browser.close();
+  const browser = await puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    headless: true,
+    args: [
+      ...chromium.args,
+      ...(proxy ? [`--proxy-server=${proxy}`] : [])
+    ],
+    defaultViewport: chromium.defaultViewport,
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://app.getgrass.io');
+  await new Promise(resolve => setTimeout(resolve, 5000));
+  await browser.close();
 }
 
 module.exports = { launchWithExtension };
- 
