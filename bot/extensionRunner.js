21ffffe6447fb580d6521549ef1a8e7cff260688
FullCorpJeff <57120894+FullCorpJeff@users.noreply.github.com>
2025-05-02 16:30:06 +0800

    grass11

--- bot/extensionRunner.js
+++ bot/extensionRunner.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 async function launchWithExtension(proxy) {
     const browser = await puppeteer.launch({
         headless: "new",
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
